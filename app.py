3f0c3909e2557c2e7ef89aa30dd95c3abb16cf43
Mike37-1 <mt241063@fhstp.ac.at>
2025-08-29 13:07:36 +0200

    Recommendation System implemented as Web-Application. Values as sliders. Genre selection from list. Only missing styling

--- app.py
+++ app.py
@@ -1,4 +1,82 @@
-import flask
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.neighbors import NearestNeighbors
 
-app = new Flask()
+#Creating Model.
+#Importing Data
+df=pd.read_csv('Files/high_popularity_spotify_data.csv')
+#Selecting Data
+numerical_features = ["energy", "danceability", "valence"]
+categorical_features = ["playlist_genre"]
+#Preproess Data
+#Numerical Data
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df[numerical_features])
+#Categorical Data
+encoder = OneHotEncoder()
+encoded_data = encoder.fit_transform(df[categorical_features]).toarray()
+#Combine Data
+processed_data = np.hstack([scaled_data, encoded_data])
+#Create Model
+model = NearestNeighbors(n_neighbors=1)
+model.fit(processed_data)
 
+#model for random genre
+model_random = NearestNeighbors(n_neighbors=1)
+model_random.fit(scaled_data)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return render_template("index.html")
+
+@app.route("/get_genres", methods =["POST"])
+
+def get_genres():
+    genres = df["playlist_genre"].unique().tolist()
+    return jsonify(genres)
+
+@app.route("/get_recommendation", methods =["POST"])
+
+def get_recommendation():
+    #get user indput data
+    print(request.get_json())
+    print('gettin recommendation')
+    user_input = pd.DataFrame([request.get_json()])
+    #Preprocess user input data
+    scaled_input = scaler.transform(user_input[numerical_features])
+    print(user_input["playlist_genre"][0])
+
+    if user_input["playlist_genre"][0] == "random":    
+        processed_input = scaled_input
+        distances, indices = model_random.kneighbors(processed_input)
+        recommendation = df.iloc[indices[0][0]]
+    elif user_input["playlist_genre"][0] != "random":
+        encoded_input = encoder.transform(user_input[categorical_features]).toarray()
+        processed_input = np.hstack([scaled_input, encoded_input])
+        #get recommendation 
+        distances, indices = model.kneighbors(processed_input)
+        recommendation = df.iloc[indices[0][0]]
+    response = {}
+    for key in recommendation.keys().tolist():
+        if type(recommendation[key]) == np.int64:
+            response[key] = int(recommendation[key])
+        else:
+            response[key] = recommendation[key]
+    jsonify(response)
+    return jsonify(response)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,108 @@
+const button = document.getElementById("btn");
+const energy_input = document.getElementById("energy_input");
+const danceability_input = document.getElementById("danceability_input");
+const valence_input = document.getElementById("valence_input");
+const display = document.getElementById("display");
+const extend_button = document.getElementById("extend_display");
+const extended_display = document.getElementById("extended_display");
+const select_genre = document.getElementById("genre");
+const checkout_button = document.getElementById("checkout_button");
+let extended_info = false;
+
+let genre_list = [];
+let response = ""; 
+
+//Request possible Genres from Data-Set and create Genre selection <option> elements
+document.addEventListener("DOMContentLoaded", async function(){
+    const request = await fetch("/get_genres", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const genres = await request.json();
+    for (const genre of genres.sort()){
+        genre_list += genre;
+        const option = document.createElement("option");
+        option.innerText = genre;
+        select_genre.appendChild(option);
+    }
+    select_genre.value = "random";
+})
+//Get Recommendation and Display Basic Data
+button.addEventListener("click", async function(){
+    extended_display.innerHTML ="";
+    extend_button.innerText="More Information";
+    extended_info = false;
+
+
+    const energy = energy_input.value;
+    const danceability = danceability_input.value;
+    const valence = valence_input.value;
+    const genre = select_genre.value;
+    let params = {};
+
+    //Params with random genre
+    if (genre == "random"){
+        params = {
+        energy: energy,
+        danceability: danceability,
+        valence: valence,
+        playlist_genre: "random"
+        }
+    }else{
+        params = {
+        energy: energy,
+        danceability: danceability,
+        valence: valence,
+        playlist_genre: genre
+        }
+    }
+
+    const get_recommendation = await fetch("/get_recommendation", {
+        method: "POST",
+        headers:{
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    })
+    response = await get_recommendation.json();
+    console.log(response);
+    
+    display.innerHTML = `
+    Artist: ${response.track_artist} <br>
+    Track: ${response.track_name} <br>
+    Genre: ${response.playlist_genre} <br>
+
+    --Values-- <br>
+    Energy: ${response.energy} <br>
+    Danceability: ${response.danceability} <br>
+    Valence: ${response.valence} <br>
+    `;
+
+    checkout_button.style.display = "flex";
+        
+    extend_button.style.display = "flex";
+    
+})
+
+checkout_button.addEventListener("click", function(){
+    console.log("Blub");
+    window.open(`https://open.spotify.com/intl-de/track/${response.track_id}`)
+})
+//Show more/less Info
+extend_button.addEventListener("click", function(){
+    if (extended_info == false){
+        for (let column in response){
+            extended_display.innerHTML += `${column}: ${response[column]} <br>`
+        }
+        extended_info = true;
+        extend_button.innerText = "Less Information";
+    }else{
+        extended_display.innerHTML = "";
+        extended_info = false;
+        extend_button.innerText = "More Information";
+    };
+
+
+})
